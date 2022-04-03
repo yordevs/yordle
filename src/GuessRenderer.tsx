2b9d79f128a98ba3c1669554883d43cd6fcf8ba3
Ben Silverman <bensilverman10@gmail.com>
2022-04-03 17:37:19 +0100

    Add styling to guess renderer

--- src/GuessRenderer.tsx
+++ src/GuessRenderer.tsx
@@ -1,51 +1,67 @@
 import { useEffect } from "react";
-
-import type { LetterMapping } from "./App";
+import styled from "styled-components";
 
 type Props = {
 	guesses: string[];
 	colorHistory: string[][];
 };
 
+interface ICell {
+	getColour: string;
+}
+
+const Container = styled.div`
+	display: flex;
+	gap: 10px;
+	flex-direction: column;
+`;
+
+const Row = styled.div`
+	display: flex;
+	gap: 10px;
+`;
+
+const Cell = styled.div<ICell>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	height: 75px;
+	width: 75px;
+
+	background-color: #efefef;
+	background-color: ${(props) => props.getColour};
+	border-radius: 8px;
+
+	color: white;
+	font-size: 24px;
+	font-weight: bold;
+`;
+
 export function GuessRenderer({ guesses, colorHistory }: Props) {
-	useEffect(() => {
-		console.log(colorHistory);
-	}, [colorHistory]);
-
 	function getLetterColour(guessNumber: number, letterIndex: number) {
-		if (!colorHistory[guessNumber]) return "#efefef";
+		if (!colorHistory[guessNumber]) return "#939B9F70";
 
-		return colorHistory[guessNumber][letterIndex];
+		const colors: { [key: string]: string } = {
+			yellow: "#CEB02C",
+			green: "#66A060",
+			grey: "#939B9F",
+		};
+
+		return colors[colorHistory[guessNumber][letterIndex]];
 	}
 
 	return (
-		<>
-			<p>Guess Renderer words below here</p>
-			<div>
-				{[0, 1, 2, 3, 4, 5].map((row) => (
-					<div
-						key={row}
-						style={{
-							display: "flex",
-							flexDirection: "row",
-							gap: 5,
-						}}>
-						{[0, 1, 2, 3, 4].map((char) => (
-							<div
-								key={char}
-								style={{
-									padding: 15,
-									border: "2px solid #1d1d1d",
-									backgroundColor: getLetterColour(row, char),
-								}}>
-								{guesses[row] && guesses[row][char] ? (
-									<span style={{}}>{guesses[row][char]}</span>
-								) : null}
-							</div>
-						))}
-					</div>
-				))}
-			</div>
-		</>
+		<Container>
+			{[0, 1, 2, 3, 4, 5].map((row) => (
+				<Row key={row}>
+					{[0, 1, 2, 3, 4].map((char) => (
+						<Cell key={char} getColour={getLetterColour(row, char)}>
+							{guesses[row] && guesses[row][char] ? guesses[row][char] : null}
+						</Cell>
+					))}
+				</Row>
+			))}
+		</Container>
 	);
 }
